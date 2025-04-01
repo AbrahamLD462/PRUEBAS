1e081750b65ae71cf3bc4a3386a34aeebe8fcd03
AbrahamLD <mario.lopez462@hotmail.com>
2025-04-01 15:04:41 -0600

    ejercicio de prueba instrumentación

---
+++ app/src/androidTest/java/com/mexiti/costogasolina/CalcularMontoUITest.kt
@@ -0,0 +1,34 @@
+package com.mexiti.costogasolina
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import java.text.NumberFormat
+
+class CalcularMontoUITest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calcularMonto40l_22_35(){
+        composeTestRule.setContent {
+            CostoGasolinaTheme {
+                CostGasLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Ingresa precio por litro de Gasolina")
+            .performTextInput("22.35")
+        composeTestRule.onNodeWithText("Litros")
+            .performTextInput("40")
+
+        val montoEsperado = NumberFormat.getCurrencyInstance().format(894.0)
+        composeTestRule.onNodeWithText("Monto Total: $montoEsperado").
+            assertExists("No se encontró ningun nodo (TextField) con el texto ingresado ")
+
+    }
+
+}
